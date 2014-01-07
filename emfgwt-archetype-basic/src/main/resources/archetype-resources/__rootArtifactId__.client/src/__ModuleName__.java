bd33d500debfacc29b3eab5271a2e254c4065232
ghillairet <g.hillairet@gmail.com>
2014-01-07 17:22:38 +0100

    Initial commit for emfgwt-archetype

---
+++ emfgwt-archetype-basic/src/main/resources/archetype-resources/__rootArtifactId__.client/src/__ModuleName__.java
@@ -0,0 +1,13 @@
+package ${package};
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class ${ModelName}App implements EntryPoint {
+
+    public void onModuleLoad() {
+        Window.alert("Hello World");
+    }
+
+}
+
